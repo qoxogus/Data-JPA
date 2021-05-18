ee9121948d3f7ebde7e16ceb19d06ac074f3ce54
qoxogus <a01034367055@gmail.com>
2021-05-19 08:40:49 +0900

    [ADD] 예제 도메인 모델과 동작확인]

---
+++ src/main/java/study/datajpa/entity/Team.java
@@ -0,0 +1,26 @@
+package study.datajpa.entity;
+
+import lombok.*;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter @Setter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@ToString(of = {"id", "name"})
+public class Team {
+
+    @Id @GeneratedValue
+    @Column(name = "team_id")
+    private Long id;
+    private String name;
+
+    @OneToMany(mappedBy = "team") //mappedby는 FK가 없는쪽에 걸어주는게 좋다
+    private List<Member> members = new ArrayList<>();
+
+    public Team(String name) {
+        this.name = name;
+    }
+}
