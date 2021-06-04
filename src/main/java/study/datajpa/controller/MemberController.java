e5728a50e76f32fb54d7a9e78b3cf2d0e08d6adc
qoxogus <a01034367055@gmail.com>
2021-06-04 22:03:56 +0900

    [ADD] Web확장 - 도메인 클래스 컨버터

---
+++ src/main/java/study/datajpa/controller/MemberController.java
@@ -0,0 +1,32 @@
+package study.datajpa.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+import study.datajpa.domain.entity.Member;
+import study.datajpa.repository.MemberRepository;
+
+import javax.annotation.PostConstruct;
+
+@RestController
+@RequiredArgsConstructor
+public class MemberController {
+
+    private final MemberRepository memberRepository;
+
+    @GetMapping("/members/{id}")
+    public String findMember(@PathVariable("id") Long id) {
+        Member member = memberRepository.findById(id).get();
+        return member.getUsername();
+    }
+    @GetMapping("/members2/{id}")
+    public String findMember2(@PathVariable("id") Member member) { //조회용으로만 쓰자(트랜잭션이 없는 범위에서 에[ㄴ티티를 조회, 엔티티를 변경해도 DB에 반영되지않음)
+        return member.getUsername();
+    }
+
+    @PostConstruct
+    public void init() {
+        memberRepository.save(new Member("userA"));
+    }
+}
