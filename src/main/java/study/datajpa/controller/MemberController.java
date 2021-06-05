f9827a58a1f62cb83f15f6d9d4672427060ceb84
qoxogus <a01034367055@gmail.com>
2021-06-05 22:06:37 +0900

    [ADD] WEB확장 - 페이징과 정렬

--- src/main/java/study/datajpa/controller/MemberController.java
+++ src/main/java/study/datajpa/controller/MemberController.java
@@ -1,9 +1,13 @@
 package study.datajpa.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+import study.datajpa.domain.dto.MemberDto;
 import study.datajpa.domain.entity.Member;
 import study.datajpa.repository.MemberRepository;
 
@@ -25,8 +29,19 @@ public class MemberController {
         return member.getUsername();
     }
 
-    @PostConstruct
+    @GetMapping("/members")
+    public Page<MemberDto> list(@PageableDefault(size = 5) Pageable pageable) {
+        Page<Member> page = memberRepository.findAll(pageable);
+//        return page.map(member -> new MemberDto(member.getId(), member.getUsername(), null));
+//        return page.map(member -> new MemberDto(member));
+        return page.map(MemberDto::new);
+    }
+
+    @PostConstruct //스프링애플리케이션이 올라올때 이게 실행된다
     public void init() {
-        memberRepository.save(new Member("userA"));
+//        memberRepository.save(new Member("userA"));
+        for (int i =0; i < 100; i++) {
+            memberRepository.save(new Member("user" + i, i));
+        }
     }
 }
